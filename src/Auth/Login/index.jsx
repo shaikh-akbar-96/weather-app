edb1233fea231936ee9e815e35be865107c00d5a
Abhishek Raj Gupta <abhishekrajgupta00@gmail.com>
2023-11-25 16:43:44 +0530

    by:abhishek login and routing error fixed

--- src/Auth/Login/index.jsx
+++ src/Auth/Login/index.jsx
@@ -1,18 +1,53 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import Button from "../../Components/Button";
+import "./login.scss";
+import { AppContext } from "../../context/AppContext";
 
 const Login = () => {
+  const [currentUser, setCurrentUser] = useState({});
+  const { setUser } = useContext(AppContext);
+
   const navigate = useNavigate();
 
-  const handleClick = () => {
-    navigate("/home");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setUser({ ...currentUser });
+    navigate(`home`);
   };
+
+  const handleChange = (e) => {
+    const { value, name } = e.target;
+    if (name === "name") {
+      setCurrentUser((user) => ({ ...user, userName: value }));
+    } else if (name === "password") {
+      setCurrentUser((user) => ({ ...user, passWord: value }));
+    }
+  };
+
   return (
-    <>
-      <Button value={"Login"} handleClick={handleClick} />
-      {/* <button onClick={() => navigate("/home")}>Login</button> */}
-    </>
+    <form className="login_container" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="name"
+        value={currentUser.userName}
+        onChange={handleChange}
+        className="input"
+        placeholder="Enter username..."
+      />
+      <input
+        type="password"
+        name="passWord"
+        value={currentUser.passWord}
+        onChange={handleChange}
+        className="input"
+        placeholder="Enter password..."
+      />
+      <Button value={"Login"} isDisabled={false} />
+      <p>
+        Don't have account ? <Link to="register">Register</Link>
+      </p>
+    </form>
   );
 };
 
